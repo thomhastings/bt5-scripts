48a6d83315908e78b7982d96d0bbe551adee6efe
Thom Hastings <thom@thom.mp>
2012-05-05 11:02:23 +0900

    Fixed bug
    Fixed bug with appending opposed to overwriting

--- get-bt5-repos.sh
+++ get-bt5-repos.sh
@@ -6,13 +6,13 @@ echo "Getting GPG key..."
 wget -q http://all.repository.backtrack-linux.org/backtrack.gpg -O- | sudo apt-key add -
 echo
 echo "Adding main repository..."
-echo "deb http://all.repository.backtrack-linux.org revolution main microverse non-free testing" > /etc/apt/sources.list
+echo "deb http://all.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list
 echo "Adding 32 bit repository..."
-echo "deb http://32.repository.backtrack-linux.org revolution main microverse non-free testing" > /etc/apt/sources.list
+echo "deb http://32.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list
 echo "Adding 64 bit repository..."
-echo "deb http://64.repository.backtrack-linux.org revolution main microverse non-free testing" > /etc/apt/sources.list
+echo "deb http://64.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list
 echo "Adding source repository..."
-echo "deb http://source.repository.backtrack-linux.org revolution main microverse non-free testing" > /etc/apt/sources.list
+echo "deb http://source.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list
 echo
 echo "Updating sources..."
 apt-get update
