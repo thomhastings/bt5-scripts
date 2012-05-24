070495b7492c5b7a9b33b22363fd89bd2603a9fa
Thom Hastings <thom@thom.mp>
2012-05-25 05:03:01 +0900

    Wallpapers
    Yeah, I did it-- adds some wallpapers via wget

--- after-install.sh
+++ after-install.sh
@@ -31,6 +31,15 @@ dpkg -i download\?dl\=packages%2Fubuntu%2Fdropbox_1.4.0_i386.deb
 echo "Cleaning up..."
 rm download\?dl\=packages%2Fubuntu%2Fdropbox_1.4.0_i386.deb
 echo
+echo "Grabbing a few wallpapers..."
+cd /usr/share/wallpapers/backtrack/
+wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/Fucking-ninja-is-Bad-ass.jpg
+wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/matrix_hacker_by_zahid4world-d3dyf5r2.jpg
+wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/miscellaneous-95767.jpeg
+wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/wallpaper-1000423.png
+wget http://pip.cat/bloc/wp-content/uploads/2012/05/hacker_manifesto_mentor.jpg
+wget http://www.wallpaper4me.com/images/wallpapers/sexyteacher-376799.jpeg
+echo
 echo "Updating \e[31mBack|Track\e[0m..."
 cd /pentest/scripts
 python bt5up.py
