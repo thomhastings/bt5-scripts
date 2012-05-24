2658eafd58a18d945fa69f4a1f6847095575227c
Thom Hastings <thom@thom.mp>
2012-05-25 04:05:03 +0900

    Typo
    fixes typo

--- after-install.sh
+++ after-install.sh
@@ -17,7 +17,7 @@ echo
 echo -e "Getting \e[33mscripts\e[0m..."
 ./get-scripts.sh
 echo
-echo -e "Installing \e[33mYakuake[e[0m..."
+echo -e "Installing \e[33mYakuake\e[0m..."
 apt-get install yakuake -y
 echo
 echo -e "Downloading & installing \e[36mDropbox\e[0m..."
