74466d891041f1fcc57f7c8d52260cfdb17bffd8
Thom Hastings <thom@attackvector.org>
2012-10-29 15:45:37 -0400

    Removed get-wifite.sh; BT5R3 includes wifite by default. start-armitage.sh also deprecated

--- after-install.sh
+++ after-install.sh
@@ -2,9 +2,6 @@
 echo -e "\e[31mBack|Track 5r2\e[0m Post-Install Script"
 echo -e "\e[37m(c) Thom Hastings 2012 New BSD license\e[0m"
 echo
-echo -e "Getting \e[33mWifite\e[0m..."
-./get-wifite.sh
-echo
 echo -e "Getting \e[33mTor\e[0m..."
 ./get-tor.sh
 echo
@@ -25,11 +22,12 @@ cd ~
 echo "Checking prerequisites..."
 apt-get install python-gpgme
 echo "Downloading..."
-wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.4.0_i386.deb
+wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
 echo "Installing..."
-dpkg -i download\?dl\=packages%2Fubuntu%2Fdropbox_1.4.0_i386.deb
-echo "Cleaning up..."
-rm download\?dl\=packages%2Fubuntu%2Fdropbox_1.4.0_i386.deb
+cd .dropbox-dist
+echo "Starting Dropbox daemon in new window-- \e[33mfollow instructions!\e[0m"
+xterm dropboxd
+cd ..
 echo
 echo "Grabbing a few wallpapers..."
 cd /usr/share/wallpapers/backtrack/
