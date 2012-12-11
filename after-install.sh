8d3848a0775a3425e7611683898ce8280e64ef11
Thom Hastings <tghastings@gmail.com>
2012-12-10 20:00:08 -0600

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -34,6 +34,13 @@ echo "Installing..."
 echo -e "Starting Dropbox installer-- \e[33mfollow instructions\e[0m!"
 start ~/.dropbox-dist/dropboxd
 echo
+echo "Getting Google Chrome..."
+echo "deb http://dl.google.com/linux/deb/ stable non-free main" >> /etc/apt/sources.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt-get update
+apt-get install google-chrome-stable -y
+sed -i 's/exec.*/& --user-data-dir/g' /usr/bin/google-chrome
+echo
 echo "Grabbing a few wallpapers..."
 cd /usr/share/wallpapers/backtrack/
 wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/Fucking-ninja-is-Bad-ass.jpg
@@ -43,40 +50,26 @@ wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/wallpaper-1000423.png
 wget http://pip.cat/bloc/wp-content/uploads/2012/05/hacker_manifesto_mentor.jpg
 wget http://www.wallpaper4me.com/images/wallpapers/sexyteacher-376799.jpeg
 echo
-echo "Getting a GRUB theme..."
-cd ~
+echo "Customizing GRUB and BootSplash..."
+#--- GRUB theme
+cd /usr/local/src/
 wget http://dl.dropbox.com/u/80562560/grub%20matrix%20theme/grub2-matrix-theme.tar.gz
 tar xvf grub2-matrix-theme.tar.gz
-cd matrix
-echo "Installing..."
-./install.sh
-echo "Disabling GRUB timeout..."
-sed -i 's/GRUB_HIDDEN_TIMEOUT=.*/GRUB_HIDDEN_TIMEOUT=5/' /etc/default/grub
-echo "Updating GRUB..."
-update-grub
-echo "Cleaning up..."
-cd ..
-rm -rf matrix/
-rm grub2-matrix-theme.tar.gz
-echo
-echo "Getting a bootsplash theme..."
-cd /opt/bootsplash/
-mkdir themes
-cd themes
-echo "Downloading theme..."
+cd matrix/
+bash install.sh #y
+rm -rf /usr/local/src/{matrix/,grub2-matrix-theme.tar.gz}
+#--- bootsplash theme
+cd /usr/local/src/
+if [ ! -e /usr/local/src/bootsplash-3.1.tar.bz2 ]; then wget ftp://ftp.bootsplash.org/pub/bootsplash/rpm-sources/bootsplash/bootsplash-3.1.tar.bz2; fi
+tar xvjf bootsplash-3.1.tar.bz2
+cd bootsplash-*/Utilities/
+make splash
+mkdir -p /opt/bootsplash/themes/
+cd /opt/bootsplash/themes/
 wget http://matrixsplash.altervista.org/matrix-splash-v1.0.tar.gz
-echo "Decompressing theme..."
 tar xvf matrix-splash-v1.0.tar.gz
-cd ..
-echo "Downloading bootsplash..."
-wget ftp://ftp.bootsplash.org/pub/bootsplash/rpm-sources/bootsplash/bootsplash-3.1.tar.bz2
-tar xvf bootsplash-3.1.tar.bz2
-cd bootsplash-3.1
-cd Utilities
-echo "Compiling bootsplash..."
-make
-echo "Writing config file.."
-echo "# config file version" >> matrix-bootsplash-1024x768.cfg
+rm matrix-splash-v1.0.tar.gz
+echo "# config file version" > matrix-bootsplash-1024x768.cfg
 echo "version=3" >> matrix-bootsplash-1024x768.cfg
 echo >> matrix-bootsplash-1024x768.cfg
 echo "# should the picture be displayed?" >> matrix-bootsplash-1024x768.cfg
@@ -99,17 +92,9 @@ echo "jpeg=/opt/bootsplash/themes/matrix-splash/images/bootsplash-1024x768.jpg" 
 echo "silentjpeg=/opt/bootsplash/themes/matrix-splash/images/silent-1024x768.jpg" >> matrix-bootsplash-1024x768.cfg
 echo "progress_enable=1" >> matrix-bootsplash-1024x768.cfg
 echo "overpaintok=1" >> matrix-bootsplash-1024x768.cfg
-echo "Applying new splash..."
-./splash -s -f matrix-bootsplash-1024x768.cfg > /opt/bootsplash/bootsplash.matrix
-cp /opt/bootsplash/bootsplash /opt/bootsplash/bootsplash.old-bt5
-cp /opt/bootsplash/bootsplash.matrix /opt/bootsplash/bootsplash
-echo "Running fix-splash..."
+cp -n /opt/bootsplash/bootsplash{,.bkup}
+/usr/local/src/bootsplash-*/Utilities/splash -s -f matrix-bootsplash-1024x768.cfg > /opt/bootsplash/bootsplash
 fix-splash
-cd ..
-cd ..
-echo "Cleaning up..."
-rm -rf bootsplash-3.1
-rm bootsplash-3.1.tar.bz2
 cd ~
 echo
 echo "Getting Audacious..."
