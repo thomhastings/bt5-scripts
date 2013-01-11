1c00f328d324070a957c9c22b6420b1355627ebd
g0tmi1k <g0tmi1k@internet.com>
2013-01-11 21:37:37 +0000

    It now works!
    Fixed GRUB & Bootsplash
    Added plymouth & Wallpaper

--- matrix-boot.sh
+++ matrix-boot.sh
@@ -1,73 +1,117 @@
 #!/bin/sh
-# chunks of code courtesy of g0tmi1k
-echo "Set Matrix Boot (Splash & GRUB) script"
+echo "Set Matrix 'Theme' (GRUB, Bootsplash, Plymouth & wallpaper) script"
 echo "(c) Thom Hastings 2013 New BSD license"
+echo "Original author: g0tmi1k"
 echo
-echo -e "Installing \e[32mcmatrix\e[0m just for fun"
-apt-get install cmatrix -y
-echo
+
+
+#--- GRUB
 echo -e "Customizing \e[33mGRUB\e[0m and \e[33mBootSplash\e[0m..."
-#--- GRUB theme
 cd /usr/local/src/
+
 echo -e "Downloading \e[33mGRUB\e[0m theme..."
-wget http://dl.dropbox.com/u/80562560/grub%20matrix%20theme/grub2-matrix-theme.tar.gz
+if [ ! -e /usr/local/src/grub2-matrix-theme.tar.gz ]; then wget http://dl.dropbox.com/u/80562560/grub%20matrix%20theme/grub2-matrix-theme.tar.gz; fi
+
 echo "Decompressing..."
-tar xvf grub2-matrix-theme.tar.gz
+tar xf grub2-matrix-theme.tar.gz
 cd matrix/
+
 echo -e "Installing... \e[33mtype 'y'\e[0m"
-sh install.sh
+bash install.sh
+
+echo -e "Altering \e[33mGRUB\e[0m settings (So we see it!)..."
+if [ ! -e /lib/plymouth/themes/simple/bt5_1024x768.png.bkup ]; then cp -f /etc/default/grub{,.bkup}; fi
+sed -i 's/^GRUB_HIDDEN_TIMEOUT=/#GRUB_HIDDEN_TIMEOUT=/' /etc/default/grub
+sed -i 's/.*GRUB_HIDDEN_TIMEOUT_QUIET=.*/GRUB_HIDDEN_TIMEOUT_QUIET=false/' /etc/default/grub
+
+echo "Running:  \e[33mupdate-grub\e[0m..."
+update-grub
+
 echo "Cleaning up..."
 rm -rf /usr/local/src/{matrix/,grub2-matrix-theme.tar.gz}
-echo "Running update-grub..."
-update-grub
 echo
-#--- bootsplash theme
-echo -e "Customizing \e[33mBootSplash\e[0m theme:"
+
+
+#--- Bootsplash
+echo -e "Customizing \e[33mBootSplash\e[0m theme..."
 cd /usr/local/src/
+
 echo -e "Downloading \e[33mBootSplash\e[0m..."
 if [ ! -e /usr/local/src/bootsplash-3.1.tar.bz2 ]; then wget ftp://ftp.bootsplash.org/pub/bootsplash/rpm-sources/bootsplash/bootsplash-3.1.tar.bz2; fi
+
 echo "Decompressing..."
-tar xvjf bootsplash-3.1.tar.bz2
+tar xjf bootsplash-3.1.tar.bz2
 cd bootsplash-*/Utilities/
-echo "Compiling..."
+
+echo "Compiling \e[33mbootsplash\e[0m theme..."
 make splash
-echo "Cleaning up..."
-rm -rf /usr/local/src/{bootsplash-3.1/,bootsplash-3.1.tar.bz2}
+
 echo "Downloading theme..."
 mkdir -p /opt/bootsplash/themes/
 cd /opt/bootsplash/themes/
-wget http://matrixsplash.altervista.org/matrix-splash-v1.0.tar.gz
+if [ ! -e /usr/local/src/matrix-splash-v1.0.tar.gz ]; then wget http://matrixsplash.altervista.org/matrix-splash-v1.0.tar.gz; fi
+
 echo "Decompressing..."
-tar xvf matrix-splash-v1.0.tar.gz
-rm matrix-splash-v1.0.tar.gz
-echo "Writing config file..."
-echo "# config file version" > matrix-bootsplash-1024x768.cfg
-echo "version=3" >> matrix-bootsplash-1024x768.cfg
-echo >> matrix-bootsplash-1024x768.cfg
-echo "# should the picture be displayed?" >> matrix-bootsplash-1024x768.cfg
-echo "state=1" >> matrix-bootsplash-1024x768.cfg
-echo >> matrix-bootsplash-1024x768.cfg
-echo "# fgcolor is the text forground color." >> matrix-bootsplash-1024x768.cfg
-echo "# bgcolor is the text background (i.e. transparent) color." >> matrix-bootsplash-1024x768.cfg
-echo "fgcolor=7" >> matrix-bootsplash-1024x768.cfg
-echo "bgcolor=0" >> matrix-bootsplash-1024x768.cfg
-echo >> matrix-bootsplash-1024x768.cfg
-echo "# (tx, ty) are the (x, y) coordinates of the text window in pixels." >> matrix-bootsplash-1024x768.cfg
-echo "# tw/th is the width/height of the text window in pixels." >> matrix-bootsplash-1024x768.cfg
-echo "tx=80" >> matrix-bootsplash-1024x768.cfg
-echo "ty=140" >> matrix-bootsplash-1024x768.cfg
-echo "tw=865" >> matrix-bootsplash-1024x768.cfg
-echo "th=560" >> matrix-bootsplash-1024x768.cfg
-echo >> matrix-bootsplash-1024x768.cfg
-echo "# name of the picture file (full path recommended)" >> matrix-bootsplash-1024x768.cfg
-echo "jpeg=/opt/bootsplash/themes/matrix-splash/images/bootsplash-1024x768.jpg" >> matrix-bootsplash-1024x768.cfg
-echo "silentjpeg=/opt/bootsplash/themes/matrix-splash/images/silent-1024x768.jpg" >> matrix-bootsplash-1024x768.cfg
-echo "progress_enable=1" >> matrix-bootsplash-1024x768.cfg
-echo "overpaintok=1" >> matrix-bootsplash-1024x768.cfg
-cp -n /opt/bootsplash/bootsplash{,.bkup}
+tar xf matrix-splash-v1.0.tar.gz
+cd matrix-splash/
+
 echo "Installing..."
-/usr/local/src/bootsplash-*/Utilities/splash -s -f matrix-bootsplash-1024x768.cfg > /opt/bootsplash/bootsplash
-echo "Running fix-splash..."
+if [ ! -e /lib/plymouth/themes/simple/bt5_1024x768.png.bkup ]; then cp -f /opt/bootsplash/bootsplash{,.bkup}; fi
+if [ ! -e /lib/plymouth/themes/simple/bt5_1024x768.png.bkup ]; then cp -f /opt/bootsplash/themes/matrix-splash/config/bootsplash-1024x768.cfg{,.bkup}; fi
+sed -i 's/^jpeg=.*/jpeg=\/opt\/bootsplash\/themes\/matrix-splash\/images\/bootsplash-1024x768.jpg/' /opt/bootsplash/themes/matrix-splash/config/bootsplash-1024x768.cfg
+sed -i 's/^silentjpeg=.*/silentjpeg=\/opt\/bootsplash\/themes\/matrix-splash\/images\/silent-1024x768.jpg/' /opt/bootsplash/themes/matrix-splash/config/bootsplash-1024x768.cfg
+/usr/local/src/bootsplash-*/Utilities/splash -s -f /opt/bootsplash/themes/matrix-splash/config/bootsplash-1024x768.cfg > /opt/bootsplash/bootsplash
+
+echo "Running: \e[33mfix-splash\e[0m..."
 fix-splash
+
+echo "Cleaning up..."
+#rm -rf /usr/local/src/{bootsplash-3.1/,bootsplash-3.1.tar.bz2}
+rm -rf /opt/bootsplash/themes/matrix-splash-v1.0.tar.gz
+
+
+#--- Plymouth
+echo -e "Customizing \e[33mPlymouth\e[0m theme..."
+cd /lib/plymouth/themes/simple/
+
+echo -e "Downloading \e[33mPlymouth\e[0m..."
+if [ ! -e /lib/plymouth/themes/simple/bt5_1024x768.png.bkup ]; then cp -f /lib/plymouth/themes/simple/bt5_1024x768.png{,.bkup}; fi
+if [ ! -e /lib/plymouth/themes/simple/miscellaneous-95767.jpeg ]; then wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/miscellaneous-95767.jpeg; fi #-O bt5_1024x768.png
+
+echo "Converting plymouth..."
+mogrify -density 72x72 -units PixelsPerInch miscellaneous-95767.jpeg
+convert -resize 1024x768! miscellaneous-95767.jpeg /lib/plymouth/themes/simple/bt5_1024x768.png
+
+echo "Applying image..."
+update-alternatives --auto default.plymouth #update-alternatives --config default.plymouth
+update-initramfs -u
+
+echo "Running: \e[33mfix-splash\e[0m..."
+fix-splash
+
+echo "Cleaning up..."
+rm -rf /lib/plymouth/themes/simple/miscellaneous-95767.jpeg
+
+
+#--- Wallpaper (Need to restart xserver for it to take effect)
+echo -e "Customizing \e[33mWallpaper\e[0m theme..."
+cd /usr/share/wallpapers/backtrack/
+
+echo -e "Downloading \e[33mPlymouth\e[0m..."
+if [ ! -e /usr/share/wallpapers/backtrack/hacker_manifesto_mentor.jpg ]; then wget http://pip.cat/bloc/wp-content/uploads/2012/05/hacker_manifesto_mentor.jpg; fi
+
+echo "Applying wallpaper..."
+rm -f /root/Desktop/backtrack-install.desktop
+gconftool-2 --type string --set /desktop/gnome/background/picture_filename /usr/share/wallpapers/backtrack/hacker_manifesto_mentor.jpg
+
+
+#--- cmatrix
+echo -e "Installing \e[32mcmatrix\e[0m just for fun"
+apt-get -y install cmatrix
+echo
+
+
+#--- DONE!
 echo
 echo -e "\e[32mDONE\e[0m"
+#reboot
