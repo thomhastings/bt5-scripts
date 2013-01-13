63f276b7b033b9b1581f10a85596e52adec82ba3
g0tmi1k <g0tmi1k@internet.com>
2013-01-13 00:24:32 +0000

    minor update for Matrix-boot.sh
    Updated the labels to match each over
    Fixed backup bug
    Added note regarding GRUB theme only work on x86 platforms

--- matrix-boot.sh
+++ matrix-boot.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-echo -e "Set Matrix 'Theme' (\e[33mGRUB\e[0m, \e[33mBootSplash\e[0m, \e[33mPlymouth\e[0m & \e[33mWallpaper\e[0m) script"
+echo -e "Set Matrix 'Theme' (\e[33mGRUB\e[0m, \e[33mBootsplash\e[0m, \e[33mPlymouth\e[0m & \e[33mWallpaper\e[0m) script"
 echo "(c) Thom Hastings 2013 New BSD license"
 echo -e "Original author: \e[37mg0tmi1k\e[0m"
 echo
 
 
 #--- GRUB
+echo -e "Customizing \e[33mGRUB\e[0m (x86 only!)..."
 cd /usr/local/src/
 
-echo -e "Downloading \e[33mGRUB\e[0m theme..."
+echo -e "Downloading \e[33mGRUB\e[0m..."
 if [ ! -e /usr/local/src/grub2-matrix-theme.tar.gz ]; then wget http://dl.dropbox.com/u/80562560/grub%20matrix%20theme/grub2-matrix-theme.tar.gz; fi
 
 echo "Decompressing..."
 tar xf grub2-matrix-theme.tar.gz
 cd matrix/
 
-echo -e "Installing... \e[33mfollow instructions\e[0m"
+echo -e "Installing... (\e[33mfollow instructions\e[0m)"
 bash install.sh
 
 echo -e "Altering \e[33mGRUB\e[0m settings (So we see it!)..."
-if [ ! -e /lib/plymouth/themes/simple/bt5_1024x768.png.bkup ]; then cp -f /etc/default/grub{,.bkup}; fi
+if [ ! -e  /etc/default/grub.bkup ]; then cp -f /etc/default/grub{,.bkup}; fi
 sed -i 's/^GRUB_HIDDEN_TIMEOUT=/#GRUB_HIDDEN_TIMEOUT=/' /etc/default/grub
 sed -i 's/.*GRUB_HIDDEN_TIMEOUT_QUIET=.*/GRUB_HIDDEN_TIMEOUT_QUIET=false/' /etc/default/grub
 
@@ -32,10 +33,10 @@ echo
 
 
 #--- Bootsplash
-echo -e "Customizing \e[33mBootSplash\e[0m theme..."
+echo -e "Customizing \e[33mBootsplash\e[0m..."
 cd /usr/local/src/
 
-echo -e "Downloading \e[33mBootSplash\e[0m..."
+echo -e "Downloading \e[33mBootsplash\e[0m..."
 if [ ! -e /usr/local/src/bootsplash-3.1.tar.bz2 ]; then wget ftp://ftp.bootsplash.org/pub/bootsplash/rpm-sources/bootsplash/bootsplash-3.1.tar.bz2; fi
 
 echo "Decompressing..."
@@ -45,7 +46,7 @@ cd bootsplash-*/Utilities/
 echo -e "Compiling..."
 make splash
 
-echo "Downloading theme..."
+echo "Downloading \e[33mBootsplash\e[0m..."
 mkdir -p /opt/bootsplash/themes/
 cd /opt/bootsplash/themes/
 if [ ! -e /usr/local/src/matrix-splash-v1.0.tar.gz ]; then wget http://matrixsplash.altervista.org/matrix-splash-v1.0.tar.gz; fi
@@ -61,8 +62,8 @@ sed -i 's/^jpeg=.*/jpeg=\/opt\/bootsplash\/themes\/matrix-splash\/images\/bootsp
 sed -i 's/^silentjpeg=.*/silentjpeg=\/opt\/bootsplash\/themes\/matrix-splash\/images\/silent-1024x768.jpg/' /opt/bootsplash/themes/matrix-splash/config/bootsplash-1024x768.cfg
 /usr/local/src/bootsplash-*/Utilities/splash -s -f /opt/bootsplash/themes/matrix-splash/config/bootsplash-1024x768.cfg > /opt/bootsplash/bootsplash
 
-echo -e "Running \e[33mfix-splash\e[0m..."
-fix-splash # we do this at the end, do we need to do it twice?
+#echo -e "Running \e[33mfix-splash\e[0m..."
+#fix-splash
 
 echo "Cleaning up..."
 #rm -rf /usr/local/src/{bootsplash-3.1/,bootsplash-3.1.tar.bz2}
@@ -70,18 +71,18 @@ rm -rf /opt/bootsplash/themes/matrix-splash-v1.0.tar.gz
 
 
 #--- Plymouth
-echo -e "Customizing \e[33mPlymouth\e[0m theme..."
+echo -e "Customizing \e[33mPlymouth\e[0m..."
 cd /lib/plymouth/themes/simple/
 
 echo -e "Downloading \e[33mPlymouth\e[0m..."
 if [ ! -e /lib/plymouth/themes/simple/bt5_1024x768.png.bkup ]; then cp -f /lib/plymouth/themes/simple/bt5_1024x768.png{,.bkup}; fi
 if [ ! -e /lib/plymouth/themes/simple/miscellaneous-95767.jpeg ]; then wget http://www.n1tr0g3n.com/wp-content/uploads/2011/12/miscellaneous-95767.jpeg; fi #-O bt5_1024x768.png
 
-echo "Converting image..."
+echo "Converting..."
 mogrify -density 72x72 -units PixelsPerInch miscellaneous-95767.jpeg
 convert /opt/bootsplash/themes/matrix-splash/images/silent-1024x768.jpg miscellaneous-95767.jpeg -resize 1024x768! -compose blend -define compose:args=33 -composite /lib/plymouth/themes/simple/bt5_1024x768.png
 
-echo "Applying image..."
+echo "Applying..."
 update-alternatives --auto default.plymouth #update-alternatives --config default.plymouth
 update-initramfs -u
 
@@ -92,8 +93,8 @@ echo "Cleaning up..."
 rm -rf /lib/plymouth/themes/simple/miscellaneous-95767.jpeg
 
 
-#--- Wallpaper (Need to restart xserver for it to take effect)
-echo -e "Customizing \e[33mWallpaper\e[0m theme..."
+#--- Wallpaper (Might need to restart 'xserver' for it to take effect)
+echo -e "Customizing \e[33mWallpaper\e[0m ..."
 cd /usr/share/wallpapers/backtrack/
 
 echo -e "Downloading..."
@@ -105,7 +106,7 @@ gconftool-2 --type string --set /desktop/gnome/background/picture_filename /usr/
 
 
 #--- cmatrix
-echo -e "Installing \e[32mcmatrix\e[0m just for fun"
+echo -e "Installing \e[32mcmatrix\e[0m (just for fun!)"
 apt-get -y install cmatrix
 echo
 
