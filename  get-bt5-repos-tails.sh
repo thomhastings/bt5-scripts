40dd35e8715b43749f5fe9f4173160c4dd3e34e6
Thom Hastings <thom@attackvector.org>
2013-03-05 20:23:58 -0600

    Create  get-bt5-repos-tails.sh

---
+++  get-bt5-repos-tails.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+echo "Get BT5 Repositories for TAILS Script"
+echo "(c) Thom Hastings 2013 New BSD License"
+echo "works best when run w/ sudo"
+echo
+echo "Creating /etc/apt/sources.list.d/backtrack.list"
+sudo touch /etc/apt/sources.list.d/backtrack.list
+echo
+echo "Getting GPG key..."
+wget http://all.repository.backtrack-linux.org/backtrack.gpg -O- | sudo apt-key add -
+echo
+echo "Adding main repository..."
+sudo echo "deb http://all.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list.d/backtrack.list
+MACHINE_TYPE=`uname -m`
+echo "Machine type is $MACHINE_TYPE"
+if [ ${MACHINE_TYPE} == 'x86_64']
+then
+  echo "Adding 64 bit repository..."
+  sudo echo "deb http://64.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list.d/backtrack.list
+else
+  echo "Adding 32 bit repository..."
+  sudo echo "deb http://32.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list.d/backtrack.list
+fi
+echo "Adding source repository..."
+sudo echo "deb http://source.repository.backtrack-linux.org revolution main microverse non-free testing" >> /etc/apt/sources.list.d/backtrack.list
+echo
+echo "Updating sources..."
+sudo apt-get update
+echo
+echo "DONE"
