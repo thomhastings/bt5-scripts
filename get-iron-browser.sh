095f84ff35e2e5f9b85715588b8633301eb556f5
Your Name <you@example.com>
2019-02-15 00:42:01 -0800

    improv

--- get-iron-browser.sh
+++ get-iron-browser.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "Get SRware Iron Browser script"
 echo "(c) Thom Hastings 2013 New BSD license"
+echo "    Add pentest-bookmarks.html"
+echo "(c) Thom Hastings 2019 GNU Affero GPL"
 echo
 echo "Downloading & installing SRware Iron..."
 cd ~
@@ -18,5 +20,11 @@ FILE="iron-installer.deb"
 wget "$URL" -O $FILE && echo "Installing..." && sudo dpkg -i $FILE
 echo "Cleaning up..."
 rm $FILE
+echo
+echo "Replacing Iron's bookmarks.html..."
+cd /usr/share/iron
+mv bookmarks.html bookmarks.html.backup
+wget -N -c https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/pentest-bookmarks/bookmarksv1.5.html -O bookmarks.html
+sleep 0.1
 echo
 echo -e "\e[32mDONE\e[0m"
